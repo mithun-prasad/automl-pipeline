e5c5955b97a30a91760622e8103fcaeaf6c39d4b
root <root@automlnlp.g0k0plzye0oe3frefn5qjtx4oa.cx.internal.cloudapp.net>
2018-12-02 16:29:43 +0000

    added create image

--- code/scoring/score.py
+++ code/scoring/score.py
@@ -1,29 +1,24 @@
 import pickle
 import json
-import numpy 
-from sklearn.ensemble import RandomForestClassifier
+import numpy
+from sklearn.externals import joblib
 from azureml.core.model import Model
+import azureml.train.automl
+
+model_name = "model.pkl"
 
 def init():
     global model
-    from sklearn.externals import joblib
-    # load the model from file into a global object
-    model_path = Model.get_model_path(model_name = 'sklearn_regression_model.pkl')
+    model_path = Model.get_model_path(model_name = model_name)
+    # deserialize the model file back into a sklearn model
     model = joblib.load(model_path)
 
-def run(raw_data):
+def run(rawdata):
     try:
-        data = json.loads(raw_data)['data']
+        data = json.loads(rawdata)['data']
         data = numpy.array(data)
         result = model.predict(data)
-        return json.dumps({"result": result.tolist()})
     except Exception as e:
         result = str(e)
         return json.dumps({"error": result})
-
-if __name__ == "__main__":
-    # Test scoring
-    init()
-    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
-    prediction = run(test_row)
-    print('Test result: ',prediction)
+    return json.dumps({"result":result.tolist()})

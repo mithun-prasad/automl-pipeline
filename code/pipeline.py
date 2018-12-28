56e10ab780ac7877d275af2773df58e403aedf19
Mithun Prasad <mithunprasad@Mithuns-MacBook-Pro.local>
2018-12-28 16:44:35 +0530

    added dynamic pickup of config

--- code/pipeline.py
+++ code/pipeline.py
@@ -17,17 +17,10 @@ import json
 
 print("SDK Version:", azureml.core.VERSION)
 
-
-workspace_name = 'predmaintws'
-resource_group = 'wopauli_AD'
-subscription_id = '5be49961-ea44-42ec-8021-b728be90d58c'
-workspace_region = 'eastus2'
-
-ws = Workspace.create(name = workspace_name,
-                      subscription_id = subscription_id,
-                      resource_group = resource_group,
-                      location = workspace_region,
-                      exist_ok=True)
+ws = Workspace.from_config()
+print('Workspace name: ' + ws.name, 
+      'Subscription id: ' + ws.subscription_id, 
+      'Resource group: ' + ws.resource_group, sep = '\n')
 
 experiment_name =  'pred-maint-automl' # choose a name for experiment
 project_folder = '.' # project folder
